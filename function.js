846fc31cbf7de392efc30ab92f1aa42cf918425a
WB2302103 <ahamadrufayee890@gmail.com>
2025-08-20 13:13:56 +0600

    function_add method and calling method

--- function.js
+++ function.js
@@ -1,10 +1,40 @@
-// function declare :
-// function keyword:function name ():{}
-function jogkori()
-{
-    console.log("Jog korbo");
+// // function declare :
+// // function keyword:function name ():{}
+// function jogkori()
+// {
+//     console.log("Jog korbo");
+// }
+// // function calling 
+// // function name and ()
+// jogkori();
+// // function is always call able
+// function is reusable
+// 1.Function keyword,2:function name and first bracket
+// first er bhitore parameter likbo i mean value
+// (value 1,value2) agolo hochee parameter
+// {}2nd bracket er bhitore baki kaj golo likbo
+// bracket sesh kore {} er baire function ta ke
+// call korbo ()
+function sum(x,y)
+ {
+     const result=x+y;
+     return result;
 }
-// function calling 
-// function name and ()
-jogkori();
-// function is always call able
+// // function call
+// using direct function name:
+//  console.log(sum(5,3));
+//  calling by a new variable:
+ const total=sum(5,3);
+ console.log(total);
+
+// function machertorkari(mach,sobji,mosla)
+// {
+//     console.log("Tel gorom");
+//     console.log("Mosla fodan");
+//     console.log("Shaksobji din");
+//     console.log(mach+"din");
+//     return "Sosadhomachertorkari";
+// }
+// // calling
+// const result=machertorkari("elis","alo","holod");
+// console.log(result);
