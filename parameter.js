0de951921bf6fcae7226cd3abfe65b278dfa5101
WB2302103 <ahamadrufayee890@gmail.com>
2025-08-20 15:40:43 +0600

    Function_parameter

---
+++ parameter.js
@@ -0,0 +1,39 @@
+function bahtkhaou()
+{
+    // console.log("hand was")
+}
+// aktah function a chaile 
+// ak ba akadhik parameter niya
+// jai but calling a 
+function square(number)
+{
+//   const borgo=number*number;
+//   console.log(number,borgo);
+
+}
+square(5);
+// console.log(square(5));
+// 5 er borgo 5*5=25;
+// console.log(square(4));
+// 4 er brogo 4*4=16;
+// function aktah function diye
+// akadhaik parameter nite parbo 
+function add(num1,num2)
+{
+    // const sum=num1+num2;
+    // console.log([num1]+["+"]+[num2]+["="]+[sum]);
+}
+// add(4,2);
+// add(2,2);
+// add(3,2);
+function divide(num1,num2)
+{
+    const division=num1/num2;
+    console.log([num1]+["/"]+[num2]+["="]+[division]);
+}
+divide(6,2);
+divide(4,2);
+divide(10,2);
+
+
+
