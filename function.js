85ea41fd2d9ba581dc5deee55c0cb4b575811d76
WB2302103 <ahamadrufayee890@gmail.com>
2025-08-20 12:36:56 +0600

    function

---
+++ function.js
@@ -0,0 +1,10 @@
+// function declare :
+// function keyword:function name ():{}
+function jogkori()
+{
+    console.log("Jog korbo");
+}
+// function calling 
+// function name and ()
+jogkori();
+// function is always call able
