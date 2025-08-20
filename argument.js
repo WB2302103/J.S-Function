b1ac6c12ed3500091f4318dfc0eae4a91a6e37b1
WB2302103 <ahamadrufayee890@gmail.com>
2025-08-20 18:13:04 +0600

    A_Function_can_be_called_by_variable_also

---
+++ argument.js
@@ -0,0 +1,9 @@
+function difference(num1,num2)
+{
+  const diff=num1-num2;
+  console.log([num1]+["-"]+[num2]+["="]+[diff]);
+}
+const my_age=20;
+const my_father_age=50;
+difference(my_father_age,my_age);
+// a function can also call by variable ly my age varibale 
