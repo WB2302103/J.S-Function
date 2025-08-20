eb2c4dfecbb45097a32ceb27c1be402be62c9e18
WB2302103 <ahamadrufayee890@gmail.com>
2025-08-20 17:58:32 +0600

    add

--- parameter.js
+++ parameter.js
@@ -28,12 +28,17 @@ function add(num1,num2)
 // add(3,2);
 function divide(num1,num2)
 {
-    const division=num1/num2;
-    console.log([num1]+["/"]+[num2]+["="]+[division]);
+    // const division=num1/num2;
+    // console.log([num1]+["/"]+[num2]+["="]+[division]);
 }
 divide(6,2);
 divide(4,2);
 divide(10,2);
 
-
+function addall(a,b,c,d,e)
+{
+    const sum=a+b+c+d+e;
+    console.log([a]+["+"]+[b]+["+"]+[c]+["+"]+[d]+["+"]+[e]+["="]+[sum]);
+}
+addall(1,2,3,4,5);
 
