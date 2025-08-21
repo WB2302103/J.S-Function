926d027e72fb8635eb8dddbe53ceebc93b246384
WB2302103 <ahamadrufayee890@gmail.com>
2025-08-21 11:50:41 +0600

    Multiplication

---
+++ task1.js
@@ -0,0 +1,7 @@
+// Take four parameters. Multiply the four numbers and then return the result
+function Multiplication(a,b,c,d)
+{
+  const result=a*b*c*d;
+  return result;
+}
+console.log(Multiplication(2,2,2,2));
