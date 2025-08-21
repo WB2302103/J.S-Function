a7508324e4288777a90ecbf30e9de2c161b58a87
WB2302103 <ahamadrufayee890@gmail.com>
2025-08-21 19:38:06 +0600

    Even_odd

---
+++ task5.js
@@ -0,0 +1,15 @@
+// Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+function odd_even(number)
+{
+
+    if(number%2===0)
+    {
+        return "Even";
+    }
+    else{
+        
+      return "ODD";
+    }
+
+}
+console.log(odd_even(10));
