8336ae419281486b24baa6b02604007cfef8a35d
WB2302103 <ahamadrufayee890@gmail.com>
2025-08-21 18:19:14 +0600

    sum and array_length

---
+++ task3.js
@@ -0,0 +1,17 @@
+// Task-3
+// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+function make_avg(numbers)
+{ 
+    let sum=0;
+  for(number of numbers)
+  { 
+   sum=sum+number;
+  }
+  const array_length=numbers.length;
+  const avg=sum/array_length;
+  return avg;
+}
+
+console.log(make_avg([1,2,3,4]));
+
+
