59f902ed64b7541a771fc1a702346f7fdc860f7b
WB2302103 <ahamadrufayee890@gmail.com>
2025-08-21 12:12:56 +0600

    Odd_Even

---
+++ task2.js
@@ -0,0 +1,15 @@
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+function Oddeven(number)
+{
+  if(number%2!=0)
+  {
+   const nm= number*2;
+   return nm;
+  }
+  else{
+   const r= number/2;
+   return r;
+  } 
+}
+console.log(Oddeven(44));
