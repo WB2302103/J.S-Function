939dc2f66c6850db9e5ddb928836b5862ef8ae7a
WB2302103 <ahamadrufayee890@gmail.com>
2025-08-22 16:07:56 +0600

    this js auto functions

---
+++ math.js
@@ -0,0 +1,27 @@
+// function Min(numbers,max,pi,r)
+// {
+//   const result=["Minimum Number:"]+[numbers]+["  "]+["Maximum_number:"]+[max]+[" "]+["Pi:"]+[pi]+[" "]+["absolute:"]+[abs]+[" "]+["Round:"]+[r];
+//   return result;
+
+// }
+// // const min=Math.min(12,15,2,4,7,1);
+// // const max=Math.max(12,15,2,4,7,1);
+// // const pi=Math.PI.toFixed(5);
+// const abs=Math.abs(5-10);
+// // mainly absolute value mainly
+// // negetive thakle positive kore dei
+// const r=Math.round((4.2));
+// console.log(Min(r));
+
+console.log(Math.round(4.2));
+// it' mainly forcefully upadte the value
+// ceil where we see 2.25 mainly we take 2
+// but in ceil it will take 3
+console.log(Math.ceil(2.25));
+
+// in floor we see 5.95 expected is 6 but it take 5
+console.log(Math.floor(5.95));
+
+console.log(Math.random()*10);
+// for take the norm number we take round
+console.log(Math.round(Math.random()*10));
